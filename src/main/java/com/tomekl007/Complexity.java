d58da4f83b58bcb18d1366e3fcb3f739826ec519
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-11-28 18:54:08 +0100

    description of complexities

--- src/main/java/com/tomekl007/Complexity.java
+++ src/main/java/com/tomekl007/Complexity.java
@@ -5,11 +5,18 @@ import java.util.Arrays;
 public class Complexity {
 
     //O(1)
+    //This function runs in O(1) time (or "constant time") relative to its input.
+    //The input array could be 1 item or 1,000 items,
+    //but this function would still just require one "step."
     public static void printFirstItem(int[] arrayOfItems) {
         System.out.println(arrayOfItems[0]);
     }
 
     //O(N)
+    //This function runs in O(n) time (or "linear time"),
+    // where n is the number of items in the array.
+    // If the array has 10 items, we have to print 10 times.
+    // If it has 1,000 items, we have to print 1,000 times
     public static void printAllItems(int[] arrayOfItems) {
         for (int item : arrayOfItems) {
             System.out.println(item);
@@ -17,6 +24,14 @@ public class Complexity {
     }
 
     //O(N^2)
+    /*
+    Here we're nesting two loops.
+    If our array has n items, our outer loop runs n times and our inner loop runs n times for each iteration
+    of the outer loop, giving us n^2​​  total prints.
+    Thus this function runs in O(n^2) time (or "quadratic time").
+    If the array has 10 items, we have to print 100 times.
+    If it has 1,000 items, we have to print 1,000,000 times
+     */
     public static void printAllPossibleOrderedPairs(int[] arrayOfItems) {
         for (int firstItem : arrayOfItems) {
             for (int secondItem : arrayOfItems) {
@@ -27,12 +42,16 @@ public class Complexity {
     }
 
     //O(N) + O(N) = O(N)
+    /*
+    When you're calculating the big O complexity of something,
+    you just throw out the constants.
+    So like:
+     */
     public static void printAllItemsTwice(int[] theArray) {
         for (int item : theArray) {
             System.out.println(item);
         }
 
-        // once more, with feeling
         for (int item : theArray) {
             System.out.println(item);
         }
